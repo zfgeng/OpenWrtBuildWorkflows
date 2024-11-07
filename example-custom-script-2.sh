6aec1bc28e236e8ee968778c318f880474397ea0
zfgeng <79776797+zfgeng@users.noreply.github.com>
2024-11-07 11:13:36 +0800

    Update example-custom-script-2.sh

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -16,4 +16,16 @@
 #patch content
 #EOF
 #git apply example.patch
-
+#修改immortalwrt.lan关联IP
+sed -i 's/192.168.1.1/192.168.211.1/g' $(find feeds/luci/modules/luci-mod-system -type f -name "flash.js")
+#修改默认IP地址
+sed -i 's/192.168.1.1/192.168.211.1/g' package/base-files/files/bin/config_generate
+##-----------------Add OpenClash dev core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+##-----------------Delete DDNS's examples-----------------
+sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns

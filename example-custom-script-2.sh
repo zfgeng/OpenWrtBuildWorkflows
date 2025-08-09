42e4d7d69d19a9ff337b8d3edf9cb94730f52693
zfgeng <79776797+zfgeng@users.noreply.github.com>
2025-08-09 11:02:13 +0800

    Update example-custom-script-2.sh

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -26,10 +26,11 @@ git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull --depth 1 origin master
 git branch --set-upstream-to=origin/master master
+cd..
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p luci-app-openclash/root/etc/openclash/core
+mkdir -p package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------

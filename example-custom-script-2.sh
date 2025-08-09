bb247e360fd19e6a06c70b5aec0339ff10af10c9
zfgeng <79776797+zfgeng@users.noreply.github.com>
2025-08-09 10:49:39 +0800

    Update example-custom-script-2.sh

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -29,7 +29,7 @@ git branch --set-upstream-to=origin/master master
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
+mkdir -p luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------

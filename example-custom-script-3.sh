69f6d6a7266abc2be91d05d3dc4fc6556526b5d6
zfgeng <79776797+zfgeng@users.noreply.github.com>
2025-08-12 16:01:55 +0800

    Create example-custom-script-3.sh

---
+++ example-custom-script-3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright (c) 2022-2023 Curious <https://www.curious.host>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# 
+# https://github.com/Curious-r/OpenWrtBuildWorkflows
+# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+#-------------------------------------------------------------------------------------------------------
+#
+#
+# Patching is generally recommended.
+# # Here's a template for patching:
+#touch example.patch
+#cat>example.patch<<EOF
+#patch content
+#EOF
+#git apply example.patch
+##-----------------Add Luci-app-openclash and dev core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+##-----------------Delete DDNS's examples-----------------
+sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
+##-----------------Add Luci-app-modns-----------------
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+##-----------------Add Luci-app-gecoosac-----------------
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
